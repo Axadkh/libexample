b15107d150ee634a2689bc017b26bb3c1c413caf
=Axadkh <asad.u88@hotmail.com>
2021-06-26 09:26:08 -0700

    first commit

---
+++ logutil/src/main/java/com/example/logutil/SomeLibCls.java
@@ -0,0 +1,22 @@
+package com.example.logutil;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.widget.ImageView;
+
+
+
+public class SomeLibCls  extends androidx.appcompat.widget.AppCompatImageView {
+
+
+    public SomeLibCls(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+    }
+}
